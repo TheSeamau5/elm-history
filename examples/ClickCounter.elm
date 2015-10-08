51f24257e7d3e8a6281161ae9f3894ac3721f84d
Ryan Rempel <rgrempel@gmail.com>
2015-10-07 20:45:02 -0500

    Update for Elm 0.16.

--- examples/ClickCounter.elm
+++ examples/ClickCounter.elm
@@ -1,18 +1,18 @@
 import History
 import Graphics.Element exposing (Element, show)
 import Mouse
-import Signal exposing ((<~))
+import Signal exposing (Signal)
 import Task exposing (Task)
 
 makeTitle n = "n = " ++ toString n
 
 port title : Signal String
-port title = makeTitle <~ counts
+port title = Signal.map makeTitle counts
 
 makeHash n = "#" ++ toString n
 
 port runTask : Signal (Task error ())
-port runTask = History.setPath <~ (makeHash <~ counts)
+port runTask = Signal.map History.setPath (Signal.map makeHash counts)
 
 update : () -> Int -> Int
 update () n = n + 1
@@ -21,4 +21,4 @@ counts : Signal Int
 counts = Signal.foldp update 0 Mouse.clicks
 
 main : Signal Element
-main = show <~ counts
+main = Signal.map show counts
